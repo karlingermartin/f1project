4e50aa0382ab5e306aed88219b145b91403b965e
Karlinger Martin <kmartin990708@gmail.com>
2022-10-19 23:30:52 +0200

    Data analysis update

--- f1app/views.py
+++ f1app/views.py
@@ -10,11 +10,10 @@ from django.views.generic import (TemplateView, ListView, DetailView, CreateView
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-#import matplotlib.dates as mdates
 import io, base64
-#from django.db.models.functions import TruncDay
-#from matplotlib.ticker import LinearLocator
 import numpy as np
+import pandas as pd
+import seaborn as sb
 
 # Create your views here.
 
@@ -177,13 +176,62 @@ class AnalysisListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        fig, ax = plt.subplots()
-        x = np.linspace(0, 2 * np.pi, 100)
-        y = np.sin(x)
-        ax.plot(x, y)
+
+        #load the data and assign columns
+        results = pd.read_csv(r"f1app\data\results.csv",sep = ',',encoding='utf-8')
+        races = pd.read_csv(r"f1app\data\races.csv",sep = ';',encoding='utf-8')
+        drivers = pd.read_csv(r"f1app\data\drivers.csv",sep = ',',encoding='utf-8')
+        constructors = pd.read_csv(r"f1app\data\constructors.csv",sep = ',',encoding='utf-8')
+
+        #merge datasets
+        df = pd.merge(results, races[["raceId", "year", "name", "round"]], on="raceId", how="left")
+        df = pd.merge(df, drivers[["driverId", "driverRef", "nationality"]], on = "driverId", how="left")
+        df = pd.merge(df, constructors[["constructorId", "name", "nationality"]], on="constructorId", how="left")
+
+        #drop, rename, rearrange coloumns
+        df.drop(["number", "position", "positionText", "laps", "fastestLap", "statusId", "resultId", "raceId", "driverId", "constructorId"], axis=1, inplace = True)
+        df.rename(columns={"rank":"fastestLapRank", "name_x":"gpName", "nationality_x":"driverNationality", "name_y":"constructorName", "nationality_y":"constructorNationality", "driverRef":"driver"}, inplace = True)
+        df = df[["year", "gpName", "round", "driver", "constructorName", "grid", "positionOrder", "points", "time", "milliseconds", "fastestLapRank", "fastestLapTime", "fastestLapSpeed", "driverNationality", "constructorNationality"]]
+
+        #drop incomplete seasons
+        df = df[df["year"]!=2021]
+        df = df[df["year"]!=2022]
+
+        #sort values
+        df = df.sort_values(by=["year", "round", "positionOrder"], ascending = [False, True, True])
+
+        #replace \N values
+        df.time.replace("\\N", np.nan, inplace = True)
+        df.milliseconds.replace("\\N", np.nan, inplace = True)
+        df.fastestLapRank.replace("\\N", np.nan, inplace = True)
+        df.fastestLapTime.replace("\\N", np.nan, inplace = True)
+        df.fastestLapSpeed.replace("\\N", np.nan, inplace = True)
+
+        #change datatypes
+        df.fastestLapSpeed = df.fastestLapSpeed.astype(float)
+        df.fastestLapRank = df.fastestLapRank.astype(float)
+        df.milliseconds = df.milliseconds.astype(float)
+
+        #reset index
+        df.reset_index(drop=True, inplace = True)
+
+        sb.set_palette("Set3")
+        plt.rcParams["figure.figsize"]=10,6
+
+        driver_winner = df.loc[df["positionOrder"]==1].groupby("driver")["positionOrder"].count().sort_values(ascending=False).to_frame().reset_index()
+
+        sb.barplot(data=driver_winner, y ="driver", x="positionOrder", color="green", alpha=0.8)
+
+        top10Drivers = driver_winner.head(10)
+        sb.barplot(data = top10Drivers, y = "driver", x="positionOrder", color="blue", alpha = 0.8, linewidth=.8, edgecolor="black")
+        plt.title("Top 10 Gp Winners")
+        plt.ylabel("Drivers")
+        plt.xlabel("Number of Wins")
+        fig = plt.figure()
 
         flike = io.BytesIO()
         fig.savefig(flike)
         b64 = base64.b64encode(flike.getvalue()).decode()
         context['chart'] = b64
+
         return context
